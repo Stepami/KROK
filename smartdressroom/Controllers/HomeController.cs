03805dfc27eed4d614b429fb2768ab82913f7a2d
Steven <Steven@DESKTOP-02NBP64>
2019-01-10 02:10:49 +0300

    Update

--- smartdressroom/Controllers/HomeController.cs
+++ smartdressroom/Controllers/HomeController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using smartdressroom.Models;
 
 namespace smartdressroom.Controllers
 {
     public class HomeController : Controller
     {
+        private List<ClothesModel> clothes;
+
+        public HomeController()
+        {
+            clothes = new List<ClothesModel>
+            {
+                new ClothesModel(1, 132, 1000, "L", "SABBAT CULT"),
+                new ClothesModel(2, 1456, 500, "M", "SELA")
+            };
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,5 +24,8 @@ namespace smartdressroom.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public string Scan(int code) => clothes.Exists(item => item.Code == code) ? "yes" : "no";
     }
 }
---
+++ smartdressroom/Models/ClothesModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smartdressroom.Models
+{
+    public class ClothesModel
+    {
+        public ClothesModel(int id, int code, int price, string size, string brand)
+        {
+            Id = id;
+            Code = code;
+            Price = price;
+            Size = size;
+            Brand = brand;
+        }
+
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public int Price { get; set; }
+        public string Size { get; set; }
+        public string Brand { get; set; }
+    }
+}
