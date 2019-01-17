c178c3b0fd23f10f2c966a465009b146479f15bc
Steven <Steven@DESKTOP-02NBP64>
2019-01-17 14:18:20 +0300

    Update

--- smartdressroom/Controllers/HomeController.cs
+++ smartdressroom/Controllers/HomeController.cs
@@ -16,24 +16,12 @@ namespace smartdressroom.Controllers
                 new ClothesModel(2, 12, 1200, "L", "SELA", "/images/clothes/sela_jemper1.jpg")
             };
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult Cart()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
+        public IActionResult Cart() => View();
 
         [HttpPost]
-        public IActionResult Product(int code)
-        {
-            if (clothes.Exists(item => item.Code == code))
-            {
-                var temp = clothes.Find(item => item.Code == code);
-                return View(temp);
-            }
-            else return Content("Scan_Error");
-        }
+        public IActionResult Product(int code) => clothes.Exists(item => item.Code == code)
+            ? View(clothes.Find(item => item.Code == code))
+            : View(new ClothesModel(0, 0, 0, "", "", "/images/scan_error.png"));
     }
 }
