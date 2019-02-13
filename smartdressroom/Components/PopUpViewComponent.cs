4ab7f6a468d096fc25f1bfa080107101dd0be87b
Steven <Steven@DESKTOP-02NBP64>
2019-02-13 19:38:15 +0300

    PopUp window

---
+++ smartdressroom/Components/PopUpViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace smartdressroom.Components
+{
+    public class PopUpViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
