ad2ff311fc404ef075e940cde8e7273e3263eae1
Steven <Steven@DESKTOP-02NBP64>
2019-07-13 01:33:15 +0300

    отказ от компонентов представления

--- smartdressroom/Components/PopUpViewComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace smartdressroom.Components
-{
-    public class PopUpViewComponent : ViewComponent
-    {
-        public IViewComponentResult Invoke() => View();
-    }
-}
