ebbbcc770181b31576fe18b67a12429aa3bb5c67
Steven <Steven@DESKTOP-02NBP64>
2019-07-15 00:46:33 +0300

    Добавлен popup вызвать консультанта

---
+++ smartdressroom/wwwroot/js/modernizr.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $('#consultant').after($('#size'));
+    $('#user').after($('#price'));
+    $('#consultant').click(function () {
+        $('#consultModal').modal('show');
+    });
+    $('#consultModalfooter').click(function () {
+        $('#consultModal').modal('hide');
+    });
+});
