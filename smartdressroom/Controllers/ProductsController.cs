2332822f1dd20990e6c2010e4d3022f912557179
StMinin <Diggerss13@yandex.ru>
2019-08-12 17:05:09 +0300

    REST API Получение вещи и смежных из коллекции

---
+++ smartdressroom/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace smartdressroom.Controllers
+{
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly Storage.ApplicationContext context;
+
+        public ProductsController(Storage.ApplicationContext context) => this.context = context;
+
+        // GET api/products/R240580
+        [HttpGet("api/[controller]/{vcode}")]
+        public async Task<object> Get(string vcode)
+        {
+            var result = await context.ClothesModels
+                .Include(cm => cm.Collection.ClothesModels)
+                .FirstOrDefaultAsync(cm => cm.VendorCode == vcode);
+
+            if (result == null)
+                return null;
+
+            var collection = result.Collection.ClothesModels
+                .Where(cm => cm.ID != result.ID)
+                .Select(cm => 
+                new
+                {
+                    cm.VendorCode,
+                    cm.Price,
+                    cm.Sizes,
+                    cm.Brand,
+                    cm.ImagesCount,
+                    cm.ImgPath
+                }).ToList();
+
+            return new
+            {
+                result.VendorCode,
+                result.Price,
+                result.Sizes,
+                result.Brand,
+                collectionName = result.Collection.Name,
+                result.ImagesCount,
+                result.ImgPath,
+                collection
+            };
+        }
+    }
+}
